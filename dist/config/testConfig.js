dbfc4222a276c03daf3464ca33f1c980986146d7
madhu <madhu.e@techostinger.com>
2025-07-26 17:58:10 +0530

    Remove dist folder from tracking

--- dist/config/testConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    baseUrl: process.env.BASE_URL || "https://uat2.makeyourjodi.com/",
-    timeout: 20000,
-    headless: false,
-};
